a240f682c26f7c3968520028b2ad6171cc2a8e6b
MiniSlime <takuminn717@gmail.com>
2022-01-20 21:41:09 +0900

    [add]トップ画面完成

---
+++ lib/utility.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+final List<Color> colorList = [const Color(0xFFFFFFFF), const Color(0xFF000000), const Color(0xFF0000FF), const Color(0xFFFF00FF),
+  const Color(0xFFFFFF00), const Color(0xFF00FFFF), const Color(0xFFFF0000), const Color(0xFF00FF00)];
+final List<String> colorNames = ['White', 'Black', 'Blue', 'Magenta', 'Yellow', 'Cyan', 'Red', 'Green'];
+final List<Image> imageList = [
+  Image.asset(
+    'assets/ring.png',
+    fit: BoxFit.contain,
+  ),
+  Image.asset(
+    'assets/ring.png',
+    fit: BoxFit.contain,
+  ),
+  Image.asset(
+    'assets/ring.png',
+    fit: BoxFit.contain,
+  ),
+  Image.asset(
+    'assets/ring.png',
+    fit: BoxFit.contain,
+  ),
+  Image.asset(
+    'assets/ring.png',
+    fit: BoxFit.contain,
+  ),
+];
